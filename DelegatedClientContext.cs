776bf5c8ff5cf4c3fb76972893a93a720bacea99
Christian Zachariasen <chrizach@gmail.com>
2016-05-02 10:31:10 +0200

    Added a DelegatedClientContext class for calling functions on behalf of a client

---
+++ DelegatedClientContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrintNode.Net
+{
+    public class PrintNodeDelegatedClientContext : IDisposable
+    {
+        public static PrintNodeDelegatedClientContext Current { get; private set; }
+        internal string ClientId { get; private set; }
+
+        public PrintNodeDelegatedClientContext(string clientId)
+        {
+            if (Current != null)
+            {
+                throw new Exception("Nested client contexts are not supported.");
+            }
+
+            ClientId = clientId;
+            Current = this;
+        }
+
+        public void Dispose()
+        {
+            Current = null;
+        }
+    }
+}
